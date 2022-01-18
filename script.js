92d3287a980e14236be42ffd9d7d346c51a57bf4
Gabriel Schettino <64440555+gabrielschettino@users.noreply.github.com>
2022-01-18 10:38:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function resetdata(){
-    
+var tip = 0.05;
 
+function resetdata(){
+    tip = 0.05;
+    
+}
+
+function customtip(){
+    tip = document.getElementsByClassName("button cyanize")[0].value / 100;
+    calculatetip();
+}
+
+function changetip(newtipvalue){
+    tip = newtipvalue;
+    calculatetip();
+}
+
+function calculatetip(){
+    var bill = document.getElementById("billvalue").value;
+    var npeople = document.getElementById("npeople").value;
+    
+    var tipamountperson = (bill * tip)/npeople;
+    var totalperson = (bill/npeople) + tipamountperson;
+    
+    console.log(totalperson);
+    console.log(tipamountperson);
+
+    
 }
 
